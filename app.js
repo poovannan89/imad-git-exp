23e511aab03e79cf6c9731e35e19dece31a8cb25
poovannan89 <poovannancse@gmail.com>
2017-09-19 16:04:51 +0000

    Half way done with feature. So far so good

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
+function aBigNewFeature()
+{
+	console.log('new feature..Lot of code');
+	console.log('Oh..half way done');
+};
 function something() {
 	console.log("im something");
 }
 console.log('hello world');
+something();
+aBigNewFeature();
